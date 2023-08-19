d8fafd7ba02ad22da28c25d6ef98409b08fdc94c
Ipsita Nayak <ipsitanayak486>
2023-08-19 19:18:06 +0530

    added project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Card from './components/cards/card'
+import Grid from './components/Grid/Grid'
+
+function App() {
+
+
+  return (
+    <>
+      <Grid numberOfCards={9}/>
+    </>
+  )
+}
+
+export default App
